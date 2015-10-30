gpg/gpgol

commit 383cb09127d268a9e49e80c013e7db29cea8ff65

Fix various warnings
* src/mapihelp.cpp (get_msgcls_from_pgp_lines): Add braces.
* src/message.cpp (message_decrypt): Add braces.
* src/oomhelp.cpp (dump_excepinfo): Remove additional format params.

src/message.cpp
@@ -1012,7 +1012,7 @@ message_decrypt (LPMESSAGE message, msgtype_t msgtype, int force, HWND hwnd)
             }
 
 
-          if (part1_idx == -1 || part2_idx == -1 
+          if ((part1_idx == -1 || part2_idx == -1)
               && !table[0].end_of_table && table[1].end_of_table
               && table[0].attach_type == ATTACHTYPE_MOSS
               && table[0].filename 

src/mapihelp.cpp
@@ -620,7 +620,7 @@ get_msgcls_from_pgp_lines (LPMESSAGE message)
 
   /* The first ~1k of the body of the message is now available in the
      utf-8 string BODY.  Walk over it to figure out its type.  */
-  for (p=body; p && *p; p = (p=strchr (p+1, '\n')? (p+1):NULL))
+  for (p=body; p && *p; p = ((p=strchr (p+1, '\n')) ? (p+1) : NULL))
     {
       if (!strncmp (p, "-----BEGIN PGP ", 15))
         {

src/oomhelp.cpp
@@ -123,8 +123,7 @@ dump_excepinfo (EXCEPINFO err)
              err.bstrSource, err.bstrDescription, err.bstrHelpFile,
              (unsigned int) err.dwHelpContext,
              (unsigned int) err.pfnDeferredFillIn,
-             (unsigned int) err.scode,
-             SRCNAME, __func__);
+             (unsigned int) err.scode);
 }
 
 /* Return the OOM object's IDispatch interface described by FULLNAME.

commit 6f2f9a3042f856ec54ec89808cb204f0b9dcd058

Fix regression introduced by 383cb091
* src/message.cpp (message_decrypt): Fix parenthesis.

--
The fix for -Wparentheses used the wrong paranthesis and
this broke detection of our "own" pgp/mime mails.

Should have been more attentive in my first semester
"basics of programming" course :-)

src/message.cpp
@@ -1008,11 +1008,11 @@ message_decrypt (LPMESSAGE message, msgtype_t msgtype, int force, HWND hwnd)
             }
 
 
-          if ((part1_idx == -1 || part2_idx == -1)
+          if (part1_idx == -1 || (part2_idx == -1
               && !table[0].end_of_table && table[1].end_of_table
               && table[0].attach_type == ATTACHTYPE_MOSS
               && table[0].filename 
-              && !strcmp (table[0].filename, MIMEATTACHFILENAME))
+              && !strcmp (table[0].filename, MIMEATTACHFILENAME)))
             {
               /* This is likely a PGP/MIME created by us.  Due to the
                  way we created that message, the MAPI derived content