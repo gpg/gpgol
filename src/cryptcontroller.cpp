gpg/gpgol

commit 7cb3feaf64d3098a5bc56cad62576bb67e5d74bf

Make S/MIME compatible with Exchange 2016 patched
* src/oomhelp.h (PR_PIDNameContentType_DASL): New.
* src/mimemaker.cpp (create_top_encryption_header): We no longer
create the header ourself.
* src/mail.cpp (Mail::update_crypt_oom): Here we set the header
values now.
* src/cryptcontroller.h (CryptController::get_protocol)
(CryptController::is_encrypter): New accessors.
* src/cryptcontroller.cpp (create_encrypt_attach): Write binary
data for S/MIME. Outlook will do the base64 conversion.

--
This fixes a lot of interop issues. Otherwise we would
either have weirdly structured mails or mails without smime-type
header or mails where the pkcs7-mime part was doubly base64
encoded with the inner encoding containing the headers.

Hope this works with older Exchanges and Outlooks.

GnuPG-Bug-Id: T3884

src/cryptcontroller.cpp
@@ -838,15 +838,8 @@ create_encrypt_attach (sink_t sink, protocol_t protocol,
       return rc;
     }
 
-  if (protocol == PROTOCOL_OPENPGP)
-    {
-      rc = write_data (sink, encryptedData);
-    }
-  else
-    {
-      const auto encStr = encryptedData.toString();
-      rc = write_b64 (sink, encStr.c_str(), encStr.size());
-    }
+  rc = write_data (sink, encryptedData);
+
   if (rc)
     {
       log_error ("%s:%s: Failed to create top header.",

src/mimemaker.cpp
@@ -1915,6 +1915,16 @@ create_top_encryption_header (sink_t sink, protocol_t protocol, char *boundary,
   else if (protocol == PROTOCOL_SMIME)
     {
       *boundary = 0;
+      rc = 0;
+      /*
+        For S/MIME encrypted mails we do not use the S/MIME conversion
+        code anymore. With Exchange 2016 this no longer works. Instead
+        we set an override mime tag, the extended headers in OOM in
+        Mail::update_crypt_oom and let outlook convert the attachment
+        to base64.
+
+        A bit more details can be found in T3853 / T3884
+
       rc = write_multistring (sink,
                               "Content-Type: application/pkcs7-mime; "
                               "smime-type=enveloped-data;\r\n"
@@ -1924,6 +1934,7 @@ create_top_encryption_header (sink_t sink, protocol_t protocol, char *boundary,
                               "MIME-Version: 1.0\r\n"
                               "\r\n",
                               NULL);
+      */
     }
   else
     {

src/oomhelp.h
@@ -117,6 +117,8 @@ DEFINE_OLEGUID(IID_IOleWindow,                0x00000114, 0, 0);
   "http://schemas.microsoft.com/mapi/proptag/0x5D01001F"
 #define PR_TAG_RECEIVED_REPRESENTING_SMTP_ADDRESS \
   "http://schemas.microsoft.com/mapi/proptag/0x5D08001F"
+#define PR_PIDNameContentType_DASL \
+  "http://schemas.microsoft.com/mapi/string/{00020386-0000-0000-C000-000000000046}/content-type/0x0000001F"
 
 #ifdef __cplusplus
 extern "C" {

src/cryptcontroller.h
@@ -64,6 +64,12 @@ public:
   /** @brief Get an inline body as std::string. */
   std::string get_inline_data ();
 
+  /** @brief Get the protocol. Valid after do_crypto. */
+  GpgME::Protocol get_protocol () const { return m_proto; }
+
+  /** @brief check weather something was encrypted. */
+  bool is_encrypter () const { return m_encrypt; }
+
 private:
   int resolve_keys ();
   int resolve_keys_cached ();

src/mail.cpp
@@ -2613,6 +2613,21 @@ Mail::update_crypt_oom()
           return;
         }
     }
+
+  if (m_crypter->get_protocol () == GpgME::CMS && m_crypter->is_encrypter ())
+    {
+      /* We put the PIDNameContentType headers here for exchange
+         because this is the only way we found to inject the
+         smime-type. */
+      if (put_pa_string (m_mailitem,
+                         PR_PIDNameContentType_DASL,
+                         "application/pkcs7-mime;smime-type=\"enveloped-data\";name=smime.p7m"))
+        {
+          log_debug ("%s:%s: Failed to put PIDNameContentType for %p.",
+                     SRCNAME, __func__, this);
+        }
+    }
+
   /** When doing async update_crypt_mapi follows and needs
     the crypter. */
   if (async_crypt_disabled ())

commit 88a3204d34b97430e7f1f80ebfe1f9f8e6fa9ddc

Change S/MIME behavior depending on Exchange ver.
* src/oomhelp.cpp, src/oomhelp.h (get_ex_major_version_for_addr): New.
* src/cryptcontroller.cpp (create_encrypt_attach): Accept
exchange version as parameter. Change behavior accordingly.
(CryptController::update_mail_mapi): Add handling for exchange ver.
* src/mimemaker.cpp (create_top_encryption_header): Accept
exchange ver.
* src/mimemaker.h: Update accordingly.

--
This fixes 7cb3feaf which broke S/MIME encrypted send for
older exchange versions and SMTP / IMAP. Because it unconditionally
used different code to construct the encrypted message in the
expectation that this would also work as an alternative.

GnuPG-Bug-Id: T3884

src/cryptcontroller.cpp
@@ -825,11 +825,12 @@ create_sign_attach (sink_t sink, protocol_t protocol,
 
 static int
 create_encrypt_attach (sink_t sink, protocol_t protocol,
-                       GpgME::Data &encryptedData)
+                       GpgME::Data &encryptedData,
+                       int exchange_major_version)
 {
   char boundary[BOUNDARYSIZE+1];
   int rc = create_top_encryption_header (sink, protocol, boundary,
-                                         false);
+                                         false, exchange_major_version);
   // From here on use goto failure pattern.
   if (rc)
     {
@@ -838,7 +839,18 @@ create_encrypt_attach (sink_t sink, protocol_t protocol,
       return rc;
     }
 
-  rc = write_data (sink, encryptedData);
+  if (protocol == PROTOCOL_OPENPGP ||
+      exchange_major_version >= 15)
+    {
+      // With exchange 2016 we have to construct S/MIME
+      // differently and write the raw data here.
+      rc = write_data (sink, encryptedData);
+    }
+  else
+    {
+      const auto encStr = encryptedData.toString();
+      rc = write_b64 (sink, encStr.c_str(), encStr.size());
+    }
 
   if (rc)
     {
@@ -886,12 +898,20 @@ CryptController::update_mail_mapi ()
   sink->cb_data = &m_input;
   sink->writefnc = sink_data_write;
 
-  // For S/MIME encrypted mails we have to use the multipart/encrypted
+  // For S/MIME encrypted mails we have to use the application/pkcs7-mime
   // content type. Otherwise newer (2016) exchange servers will throw
   // an M2MCVT.StorageError.Exeption (See GnuPG-Bug-Id: T3853 )
+
+  // This means that the conversion / build of the mime structure also
+  // happens differently.
+  int exchange_major_version = get_ex_major_version_for_addr (
+                                        m_mail->get_cached_sender ().c_str ());
+
   std::string overrideMimeTag;
-  if (m_proto == GpgME::CMS && m_encrypt)
+  if (m_proto == GpgME::CMS && m_encrypt && exchange_major_version >= 15)
     {
+      log_debug ("%s:%s: CMS Encrypt with Exchange %i activating alternative.",
+                 SRCNAME, __func__, exchange_major_version);
       overrideMimeTag = "application/pkcs7-mime";
     }
 
@@ -909,6 +929,7 @@ CryptController::update_mail_mapi ()
   protocol_t protocol = m_proto == GpgME::CMS ?
                                    PROTOCOL_SMIME :
                                    PROTOCOL_OPENPGP;
+
   int rc = 0;
   /* Do we have override MIME ? */
   const auto overrideMime = m_mail->get_override_mime_data ();
@@ -918,11 +939,11 @@ CryptController::update_mail_mapi ()
     }
   else if (m_sign && m_encrypt)
     {
-      rc = create_encrypt_attach (sink, protocol, m_output);
+      rc = create_encrypt_attach (sink, protocol, m_output, exchange_major_version);
     }
   else if (m_encrypt)
     {
-      rc = create_encrypt_attach (sink, protocol, m_output);
+      rc = create_encrypt_attach (sink, protocol, m_output, exchange_major_version);
     }
   else if (m_sign)
     {

src/mimemaker.cpp
@@ -1892,7 +1892,7 @@ sink_encryption_write_b64 (sink_t encsink, const void *data, size_t datalen)
    function.  */
 int
 create_top_encryption_header (sink_t sink, protocol_t protocol, char *boundary,
-                              bool is_inline)
+                              bool is_inline, int exchange_major_version)
 {
   int rc;
 
@@ -1915,26 +1915,31 @@ create_top_encryption_header (sink_t sink, protocol_t protocol, char *boundary,
   else if (protocol == PROTOCOL_SMIME)
     {
       *boundary = 0;
-      rc = 0;
-      /*
-        For S/MIME encrypted mails we do not use the S/MIME conversion
-        code anymore. With Exchange 2016 this no longer works. Instead
-        we set an override mime tag, the extended headers in OOM in
-        Mail::update_crypt_oom and let outlook convert the attachment
-        to base64.
+      if (exchange_major_version >= 15)
+        {
+          /*
+             For S/MIME encrypted mails we do not use the S/MIME conversion
+             code anymore. With Exchange 2016 this no longer works. Instead
+             we set an override mime tag, the extended headers in OOM in
+             Mail::update_crypt_oom and let outlook convert the attachment
+             to base64.
 
-        A bit more details can be found in T3853 / T3884
-
-      rc = write_multistring (sink,
-                              "Content-Type: application/pkcs7-mime; "
-                              "smime-type=enveloped-data;\r\n"
-                              "\tname=\"smime.p7m\"\r\n"
-                              "Content-Disposition: attachment; filename=\"smime.p7m\"\r\n"
-                              "Content-Transfer-Encoding: base64\r\n"
-                              "MIME-Version: 1.0\r\n"
-                              "\r\n",
-                              NULL);
-      */
+             A bit more details can be found in T3853 / T3884
+             */
+          rc = 0;
+        }
+      else
+        {
+          rc = write_multistring (sink,
+                                  "Content-Type: application/pkcs7-mime; "
+                                  "smime-type=enveloped-data;\r\n"
+                                  "\tname=\"smime.p7m\"\r\n"
+                                  "Content-Disposition: attachment; filename=\"smime.p7m\"\r\n"
+                                  "Content-Transfer-Encoding: base64\r\n"
+                                  "MIME-Version: 1.0\r\n"
+                                  "\r\n",
+                                  NULL);
+        }
     }
   else
     {

src/mimemaker.h
@@ -77,7 +77,7 @@ int add_body_and_attachments (sink_t sink, LPMESSAGE message,
                               mapi_attach_item_t *att_table, Mail *mail,
                               const char *body, int n_att_usable);
 int create_top_encryption_header (sink_t sink, protocol_t protocol, char *boundary,
-                              bool is_inline = false);
+                              bool is_inline = false, int exchange_major_version = -1);
 
 /* Helper to write a boundary to the output sink.  The leading LF
    will be written as well.  */

src/oomhelp.cpp
@@ -2034,3 +2034,26 @@ get_inline_body ()
 
   return body;
 }
+
+int
+get_ex_major_version_for_addr (const char *mbox)
+{
+  LPDISPATCH account = get_account_for_mail (mbox);
+  if (!account)
+    {
+      TRACEPOINT;
+      return -1;
+    }
+
+  char *version_str = get_oom_string (account, "ExchangeMailboxServerVersion");
+  gpgol_release (account);
+
+  if (!version_str)
+    {
+      return -1;
+    }
+  long int version = strtol (version_str, nullptr, 10);
+  xfree (version_str);
+
+  return (int) version;
+}

src/oomhelp.h
@@ -343,6 +343,12 @@ char *get_sender_SenderEMailAddress (LPDISPATCH mailitem);
 
 /* Get the body of the active inline response */
 char *get_inline_body (void);
+
+/* Get the major version of the exchange server of the account for the
+   mail address "mbox". Returns -1 if no version could be detected
+   or exchange is not used.*/
+int get_ex_major_version_for_addr (const char *mbox);
+
 #ifdef __cplusplus
 char *get_sender_SendUsingAccount (LPDISPATCH mailitem, bool *r_is_GSuite);
 }