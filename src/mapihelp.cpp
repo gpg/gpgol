gpg/gpgol

commit dc48589b3d429d7d156c75b4e7bc784b140f40ce

Handle weirdly constructed PGP/MIME mails
* src/mapihelp.cpp (get_msgcls_from_pgp_lines): Add return
value for empty body.
(get_msgcls_from_first_attachment): New. factored out.
(change_message_class_ipm_note): Use get_msgcls_from_first_attachment.

--
This fixes the handling of mails that are:

multipart/mixed
->application/pgp-encrypted
->application/octed-stream

They also don't have a body. To avoid overhead in a
normal unencrypted multipart mixed mail we only check
for this structure if we don't have a body.

GnuPG-Bug-Id: T3882

src/mapihelp.cpp
@@ -630,9 +630,12 @@ mapi_get_body (LPMESSAGE message, size_t *r_nbytes)
 
 /* Look at the body of the MESSAGE and try to figure out whether this
    is a supported PGP message.  Returns the new message class or NULL
-   if it does not look like a PGP message.  */
+   if it does not look like a PGP message.
+
+   If r_nobody is not null it is set to true if no body was found.
+   */
 static char *
-get_msgcls_from_pgp_lines (LPMESSAGE message)
+get_msgcls_from_pgp_lines (LPMESSAGE message, bool *r_nobody = nullptr)
 {
   HRESULT hr;
   LPSTREAM stream;
@@ -649,6 +652,11 @@ get_msgcls_from_pgp_lines (LPMESSAGE message)
       return NULL;
     }
 
+  if (r_nobody)
+    {
+      *r_nobody = false;
+    }
+
   stream = mapi_get_body_as_stream (message);
   if (!stream)
     {
@@ -660,6 +668,10 @@ get_msgcls_from_pgp_lines (LPMESSAGE message)
         {
           log_error ("%s:%s: Failed to get  w_body stream. : hr=%#lx",
                      SRCNAME, __func__, hr);
+          if (r_nobody)
+            {
+              *r_nobody = true;
+            }
           return NULL;
         }
       else
@@ -979,6 +991,30 @@ get_first_attach_mime_tag (LPMESSAGE message)
 }
 
 
+/* Look at the first attachment's content type to determine the
+   messageclass. */
+static char *
+get_msgcls_from_first_attachment (LPMESSAGE message)
+{
+  char *ret = nullptr;
+  char *attach_mime = get_first_attach_mime_tag (message);
+  if (!attach_mime)
+    {
+      return nullptr;
+    }
+  if (!strcmp (attach_mime, "application/pgp-encrypted"))
+    {
+      ret = xstrdup ("IPM.Note.GpgOL.MultipartEncrypted");
+      xfree (attach_mime);
+    }
+  else if (!strcmp (attach_mime, "application/pgp-signature"))
+    {
+      ret = xstrdup ("IPM.Note.GpgOL.MultipartSigned");
+      xfree (attach_mime);
+    }
+  return ret;
+}
+
 /* Helper for mapi_change_message_class.  Returns the new message
    class as an allocated string.
 
@@ -1022,22 +1058,7 @@ change_message_class_ipm_note (LPMESSAGE message)
       if (!newvalue)
         {
           /* So no PGP Inline. Lets look at the attachment. */
-          char *attach_mime = get_first_attach_mime_tag (message);
-          if (!attach_mime)
-            {
-              xfree (ct);
-              return nullptr;
-            }
-          if (!strcmp (attach_mime, "application/pgp-encrypted"))
-            {
-              newvalue = xstrdup ("IPM.Note.GpgOL.MultipartEncrypted");
-              xfree (attach_mime);
-            }
-          else if (!strcmp (attach_mime, "application/pgp-signature"))
-            {
-              newvalue = xstrdup ("IPM.Note.GpgOL.MultipartSigned");
-              xfree (attach_mime);
-            }
+          newvalue = get_msgcls_from_first_attachment (message);
         }
     }
   else if (!ct || !strcmp (ct, "text/plain") ||
@@ -1046,10 +1067,21 @@ change_message_class_ipm_note (LPMESSAGE message)
            !strcmp (ct, "multipart/related") ||
            !strcmp (ct, "text/html"))
     {
+      bool has_no_body = false;
       /* It is quite common to have a multipart/mixed or alternative
          mail with separate encrypted PGP parts.  Look at the body to
          decide.  */
-      newvalue = get_msgcls_from_pgp_lines (message);
+      newvalue = get_msgcls_from_pgp_lines (message, &has_no_body);
+
+      if (!newvalue && has_no_body && !strcmp (ct, "multipart/mixed"))
+        {
+          /* This is uncommon. But some Exchanges might break a PGP/MIME mail
+             this way. Let's take a look at the attachments. Maybe it's
+             a PGP/MIME mail. */
+          log_debug ("%s:%s: Multipart mixed without body found. Looking at attachments.",
+                     SRCNAME, __func__);
+          newvalue = get_msgcls_from_first_attachment (message);
+        }
     }
 
   xfree (ct);

commit aed389f9ce639db48dcfc84cb495a2b180496207

Fix crash when looking at msgs without content type
* src/mapihelp.cpp (change_message_class_ipm_note): Check
for ct before using it in string comparison.

--
The codepath is reached for temporary, discarded mails, e.g.
when a Reply All in the Inline editor is canceled through
Escape.

This fixes: dc48589b3d429d7d156c75b4e7bc784b140f40ce
GnuPG-Bug-Id: T3960

src/mapihelp.cpp
@@ -1073,7 +1073,7 @@ change_message_class_ipm_note (LPMESSAGE message)
          decide.  */
       newvalue = get_msgcls_from_pgp_lines (message, &has_no_body);
 
-      if (!newvalue && has_no_body && !strcmp (ct, "multipart/mixed"))
+      if (!newvalue && has_no_body && ct && !strcmp (ct, "multipart/mixed"))
         {
           /* This is uncommon. But some Exchanges might break a PGP/MIME mail
              this way. Let's take a look at the attachments. Maybe it's