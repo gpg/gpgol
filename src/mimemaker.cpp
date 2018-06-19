gpg/gpgol

commit 94c354a0845ba4f1e29e4bc8b9d8fc4e3b8081dc

Fix content type detection
* src/mimemaker.cpp (infer_content_type): Fix it.

--
This never worked because the tolower did not increase the
suffix pointer and just filled the buffer with the first letter
of the suffix.

Now we have c++ we can simplify the tolower.

src/mimemaker.cpp
@@ -681,24 +681,25 @@ infer_content_type (const char * /*data*/, size_t /*datalen*/,
       { 0, NULL, NULL }
     };
   int i;
-  char suffix_buffer[12+1];
-  const char *suffix;
+  std::string suffix;
 
   *force_b64 = 0;
-  suffix = filename? strrchr (filename, '.') : NULL;
-  if (suffix && strlen (suffix) < sizeof suffix_buffer -1 )
+  if (filename)
+    suffix = strrchr (filename, '.');
+
+  if (!suffix.empty())
     {
-      suffix++;
-      for (i=0; i < sizeof suffix_buffer - 1; i++)
-        suffix_buffer[i] = tolower (*(const unsigned char*)suffix);
-      suffix_buffer[i] = 0;
+      suffix.erase(0, 1);
+      std::transform(suffix.begin(), suffix.end(), suffix.begin(), ::tolower);
       for (i=0; suffix_table[i].suffix; i++)
-        if (!strcmp (suffix_table[i].suffix, suffix_buffer))
-          {
-            if (suffix_table[i].b64)
-              *force_b64 = 1;
-            return suffix_table[i].ct;
-          }
+        {
+          if (!strcmp (suffix_table[i].suffix, suffix.c_str()))
+            {
+              if (suffix_table[i].b64)
+                *force_b64 = 1;
+              return suffix_table[i].ct;
+            }
+        }
     }
 
   /* Not found via filename, look at the content.  */

commit 3ba39a0f442956ec4a6c8d0fa0e62fe696795980

Fix crash when adding attachment without fileext
* src/mimemaker.cpp (infer_content_type): Check if a suffix
was found and check for the size of the suffix before using it.

--
The crash was introduced with 94c354a0

GnuPG-Bug-Id: T4032

src/mimemaker.cpp
@@ -679,10 +679,19 @@ infer_content_type (const char * /*data*/, size_t /*datalen*/,
 
   *force_b64 = 0;
   if (filename)
-    suffix = strrchr (filename, '.');
-
-  if (!suffix.empty())
     {
+      const char *dot = strrchr (filename, '.');
+
+      if (dot)
+        {
+          suffix = dot;
+        }
+    }
+
+  /* Check for at least one char after the dot. */
+  if (suffix.size() > 1)
+    {
+      /* Erase the dot */
       suffix.erase(0, 1);
       std::transform(suffix.begin(), suffix.end(), suffix.begin(), ::tolower);
       for (i=0; suffix_table[i].suffix; i++)