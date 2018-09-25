gpg/gpgol

commit 59e8a7ee3bcd16275091c9535626e49fc2a6c4af

Use dispcache for Multilanguage COM interface
* src/dialogs.h (DISPID_MULTILANG): New.
* src/mlang-charset.cpp (ansi_charset_to_utf8): Use dispcache.

--
Using CoCreateInstance for every string to convert seemed excessive
so we better cache it.

GnuPG-Bug-Id: T4114

src/mlang-charset.cpp
@@ -29,6 +29,9 @@ DEFINE_GUID (IID_IMultiLanguage, 0x275c23e1,0x3747,0x11d0,0x9f,
 #include <mlang.h>
 #undef INITGUID
 
+#include "dialogs.h"
+#include "dispcache.h"
+
 #include "mlang-charset.h"
 
 char *ansi_charset_to_utf8 (const char *charset, const char *input,
@@ -51,9 +54,21 @@ char *ansi_charset_to_utf8 (const char *charset, const char *input,
       return xstrdup (input);
     }
 
-  CoCreateInstance(CLSID_CMultiLanguage, NULL, CLSCTX_INPROC_SERVER,
-                   IID_IMultiLanguage, (void**)&multilang);
-  memdbg_addRef (multilang);
+  auto cache = DispCache::instance ();
+  LPDISPATCH cachedLang = cache->getDisp (DISPID_MLANG_CHARSET);
+
+  if (!cachedLang)
+    {
+      CoCreateInstance(CLSID_CMultiLanguage, NULL, CLSCTX_INPROC_SERVER,
+                       IID_IMultiLanguage, (void**)&multilang);
+      memdbg_addRef (multilang);
+      cache->addDisp (DISPID_MLANG_CHARSET, (LPDISPATCH) multilang);
+    }
+  else
+    {
+      multilang = (LPMULTILANGUAGE) cachedLang;
+    }
+
 
   if (!multilang)
     {
@@ -108,7 +123,6 @@ char *ansi_charset_to_utf8 (const char *charset, const char *input,
 
   err = multilang->ConvertStringToUnicode(&mode, enc, const_cast<char*>(input),
                                           &uinlen, buf, &wlen);
-  gpgol_release (multilang);
   if (FAILED (err))
     {
       log_error ("%s:%s: Failed conversion 2.",

src/dialogs.h
@@ -75,4 +75,7 @@
 #define IDI_LEVEL_3_ENC                 (IDI_LEVEL_3 + ENCRYPT_ICON_OFFSET)
 #define IDI_LEVEL_4_ENC                 (IDI_LEVEL_4 + ENCRYPT_ICON_OFFSET)
 
+/* Other ids */
+#define DISPID_MLANG_CHARSET            0x7000
+
 #endif /*DIALOGS_H*/

commit 38f9d070bf8e32cff85c85657e9c0c86fbb66e85

Fix mlang release after mlang error
* src/mlang-charset.cpp (ansi_charset_to_utf8): Fix release.

--
In case of errors we may not release the mlang charset anymore
because we are now using it cached. This caused crashes and
probably hangs.

It is a regression from:
59e8a7ee3bcd16275091c9535626e49fc2a6c4af

GnuPG-Bug-Id: T4141

src/mlang-charset.cpp
@@ -81,7 +81,6 @@ char *ansi_charset_to_utf8 (const char *charset, const char *input,
     {
       log_error ("%s:%s: Inlen too long. Bug.",
                  SRCNAME, __func__);
-      gpgol_release (multilang);
       return NULL;
     }
 
@@ -98,7 +97,6 @@ char *ansi_charset_to_utf8 (const char *charset, const char *input,
         {
           log_error ("%s:%s: Failed to find charset for: %s",
                      SRCNAME, __func__, charset);
-          gpgol_release (multilang);
           return xstrdup (input);
         }
       enc = (mime_info.uiInternetEncoding == 0) ? mime_info.uiCodePage :
@@ -116,7 +114,6 @@ char *ansi_charset_to_utf8 (const char *charset, const char *input,
     {
       log_error ("%s:%s: Failed conversion.",
                  SRCNAME, __func__);
-      gpgol_release (multilang);
       return NULL;
   }
   buf = (wchar_t*) xmalloc(sizeof(wchar_t) * (wlen + 1));