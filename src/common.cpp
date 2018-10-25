bf4ab9a6d16869aa6083b10b54a77fc9b8b95b41
Andre Heinecke <aheinecke@intevation.de>
2018-10-25 12:23:42 +0200

    Explicitly set real access key for reg too
    * src/common.cpp (REAL_ACCESS): Define.
    
    --
    We have to make sure that we try both registry views.

--- src/common.cpp
+++ src/common.cpp
@@ -109,8 +109,10 @@ get_root_key(const char *root)
 }
 #if defined(_WIN64)
 #define CROSS_ACCESS KEY_WOW64_32KEY
+#define REAL_ACCESS KEY_WOW64_64KEY
 #else
 #define CROSS_ACCESS KEY_WOW64_64KEY
+#define REAL_ACCESS KEY_WOW32_64KEY
 #endif
 
 std::string
@@ -129,6 +131,8 @@ _readRegStr (HKEY root_key, const char *dir,
 
     if (alternate) {
         flags |= CROSS_ACCESS;
+    } else {
+        flags |= REAL_ACCESS
     }
 
     if (RegOpenKeyExA(root_key, dir, 0, flags, &key_handle)) {
