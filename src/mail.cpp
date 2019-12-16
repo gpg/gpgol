gpg/gpgol

commit 0d2db8b81ab24e2ab02d7ba6832cabd07b72f852

Enable File -> SaveAs also for mails in window
* src/gpgoladdin.cpp (GpgolRibbonExtender::GetIDsOfNames)
(GpgolRibbonExtender::Invoke)
(GetCustomUI_MIME): Add new command and wire it up.
* src/mail.cpp (Mail::checkAttachments_o): Add safeguard
not to warn about our own mime structure.
(Mail::close): Ignore hidden attachments in check.
* src/ribbon-callbacks.cpp (override_file_save_as):
Add check for inspector context and handle it differently.
(override_file_save_as_in_window): New.
* src/ribbon-callbacks.h: Update accordingly.

--
While the explorer loads a new mail object on File->saveAs
this does not happen in the inspector. So in the inspector
we have to close it to allow the write on the current object.
This is currently ugly because it will close the window and
hide the content.

Fixing the check for attachments in Mail::close is also
a real fix as otherwise our own mime strucutre attachment
would be detected.

The trick here is not to close the mail before the
save as event actually passes the event loop but right after
that.

src/mail.cpp
@@ -412,6 +412,32 @@ Mail::checkAttachments_o (bool silent)
       TRETURN 0;
     }
 
+  /* Saveguard not to warn about our own attachment */
+  if (count == 1)
+    {
+      LPDISPATCH oom_attach = get_oom_object (attachments, "Item(1)");
+      if (oom_attach)
+        {
+          char *dispName = get_oom_string (oom_attach, "DisplayName");
+          gpgol_release (oom_attach);
+
+          if (dispName && !strcmp (dispName, MIMEATTACHFILENAME))
+            {
+              xfree (dispName);
+              gpgol_release (attachments);
+              log_debug ("%s:%s: Found only our hidden mime structure.",
+                         SRCNAME, __func__);
+              TRETURN 0;
+            }
+          else if (dispName)
+            {
+              log_debug ("%s:%s: Found %s as attachment.",
+                         SRCNAME, __func__, anonstr (dispName));
+              xfree (dispName);
+            }
+        }
+    }
+
   std::string message;
 
   if (isEncrypted () && isSigned ())
@@ -2429,9 +2455,9 @@ Mail::close (bool restoreSMIMEClass)
       if (attachments)
         {
           att_count = get_oom_int (attachments, "Count");
-          gpgol_release (attachments);
         }
 
+      bool foundOne = false;
       if (body && strlen (body))
         {
           log_debug ("%s:%s: Close successful. But body found. "
@@ -2440,16 +2466,44 @@ Mail::close (bool restoreSMIMEClass)
         }
       else if (att_count)
         {
-          log_debug ("%s:%s: Close successful. But attachments found. "
-                     "Mail still open.",
-                     SRCNAME, __func__);
+          for (int i = 1; i <= att_count && !foundOne; i++)
+            {
+              std::string item_str;
+              item_str = std::string("Item(") + std::to_string (i) + ")";
+              LPDISPATCH oom_attach = get_oom_object (attachments, item_str.c_str ());
+              if (!oom_attach)
+                {
+                  log_error ("%s:%s: Failed to get attachment.",
+                             SRCNAME, __func__);
+                  continue;
+                }
+              VARIANT var;
+              VariantInit (&var);
+              if (get_pa_variant (oom_attach, PR_ATTACHMENT_HIDDEN_DASL, &var) ||
+                  (var.vt == VT_BOOL && var.boolVal == VARIANT_FALSE))
+                {
+                  foundOne = true;
+                }
+              else
+                {
+                  gpgol_release (oom_attach);
+                }
+              VariantClear (&var);
+            }
+          if (foundOne)
+            {
+              log_debug ("%s:%s: Close successful. But attachments found. "
+                         "Mail still open.",
+                         SRCNAME, __func__);
+            }
         }
-      else
+      if (!foundOne)
         {
           setPassWrite (true);
           log_debug ("%s:%s: Close successful. Next write may pass.",
                      SRCNAME, __func__);
         }
+      gpgol_release (attachments);
       xfree (body);
     }
   log_oom ("%s:%s: returned from close",

src/gpgoladdin.cpp
@@ -718,6 +718,7 @@ GpgolRibbonExtender::GetIDsOfNames (REFIID riid, LPOLESTR *rgszNames,
       ID_MAPPER (L"openContactKey", ID_CMD_OPEN_CONTACT_KEY)
       ID_MAPPER (L"overrideFileClose", ID_CMD_FILE_CLOSE)
       ID_MAPPER (L"overrideFileSaveAs", ID_CMD_FILE_SAVE_AS)
+      ID_MAPPER (L"overrideFileSaveAsWindowed", ID_CMD_FILE_SAVE_AS_IN_WINDOW)
       ID_MAPPER (L"decryptPermanently", ID_CMD_DECRYPT_PERMANENTLY)
     }
 
@@ -819,6 +820,8 @@ GpgolRibbonExtender::Invoke (DISPID dispid, REFIID riid, LCID lcid,
         return override_file_close ();
       case ID_CMD_FILE_SAVE_AS:
         return override_file_save_as (parms);
+      case ID_CMD_FILE_SAVE_AS_IN_WINDOW:
+        return override_file_save_as_in_window (parms);
       case ID_BTN_ENCRYPT:
       case ID_BTN_DECRYPT:
       case ID_BTN_DECRYPT_LARGE:
@@ -935,6 +938,10 @@ GetCustomUI_MIME (BSTR RibbonID, BSTR * RibbonXml)
       gpgrt_asprintf (&buffer,
         "<customUI xmlns=\"http://schemas.microsoft.com/office/2009/07/customui\""
         " onLoad=\"ribbonLoaded\">"
+        " <commands>"
+        "  <command idMso=\"FileSaveAs\""
+        "           onAction=\"overrideFileSaveAsWindowed\"/>"
+        " </commands>"
         " <ribbon>"
         "   <tabs>"
         "    <tab idMso=\"TabReadMessage\">"

src/ribbon-callbacks.cpp
@@ -44,6 +44,7 @@
 #include "mail.h"
 #include "dispcache.h"
 #include "addressbook.h"
+#include "windowmessages.h"
 
 #include <gpgme++/context.h>
 #include <gpgme++/data.h>
@@ -870,6 +871,28 @@ HRESULT override_file_save_as (DISPPARAMS *parms)
       TRETURN S_OK;
     }
 
+  /* Check if we are in a dedicated window which we must treat differently. */
+  LPDISPATCH context = nullptr;
+  if (parms->cArgs == 2 &&
+      getContext (parms->rgvarg[1].pdispVal, &context) == S_OK)
+    {
+      char *name = get_object_name (context);
+
+      std::string ctx_name;
+
+      if (name)
+        {
+          ctx_name = name;
+          xfree (name);
+        }
+      if (ctx_name == "_Inspector")
+        {
+          gpgol_release (context);
+          TRETURN override_file_save_as_in_window (parms);
+        }
+    }
+  gpgol_release (context);
+
   /* Do not cancel the event so that the underlying File Save As works. */
   parms->rgvarg[0].pvarVal->boolVal = VARIANT_FALSE;
   /* File->SaveAs triggers an ItemLoad event immediately after this
@@ -879,5 +902,34 @@ HRESULT override_file_save_as (DISPPARAMS *parms)
      not interfere and it can just save what is in MAPI (the encrypted
      mail). */
   g_ignore_next_load = true;
+  TRETURN S_OK;
+}
+
+HRESULT override_file_save_as_in_window (DISPPARAMS *parms)
+{
+  TSTART;
+  if (!parms)
+    {
+      STRANGEPOINT;
+      TRETURN S_OK;
+    }
+
+  /* Do not cancel the event so that the underlying File Save As works. */
+  parms->rgvarg[0].pvarVal->boolVal = VARIANT_FALSE;
+
+  LPDISPATCH ctrl = parms->rgvarg[1].pdispVal;
+
+  MY_MAIL_GETTER
+
+  if (!mail)
+    {
+      log_debug ("%s:%s: No mail.",
+                 SRCNAME, __func__);
+      TRETURN S_OK;
+    }
+
+  /* Close the mail async to allow the save as. */
+  do_in_ui_thread_async (CLOSE, mail);
+
   TRETURN S_OK;
 }

src/ribbon-callbacks.h
@@ -52,6 +52,7 @@
 #define ID_CMD_FILE_CLOSE 37
 #define ID_CMD_DECRYPT_PERMANENTLY 38
 #define ID_CMD_FILE_SAVE_AS 39
+#define ID_CMD_FILE_SAVE_AS_IN_WINDOW 40
 
 #define ID_BTN_DECRYPT           IDI_DECRYPT_16_PNG
 #define ID_BTN_DECRYPT_LARGE     IDI_DECRYPT_48_PNG
@@ -94,4 +95,7 @@ HRESULT override_file_close ();
 HRESULT decrypt_permanently (LPDISPATCH ctrl);
 /* SaveAs from the file menu */
 HRESULT override_file_save_as (DISPPARAMS *parms);
+/* Like above but for mails opened in their own window as they
+   behave differently. */
+HRESULT override_file_save_as_in_window (DISPPARAMS *parms);
 #endif

commit bbd6f516545deead2de72adc79ddf2c5706a7836

Fix check for plain text attachments
* src/mail.cpp (Mail::checkAttachments_o): Use
count_visible_attachments.
(Mail::close): Fix safeguard check.
* src/oomhelp.cpp (count_visible_attachments): New.
* src/oomhelp.h: Add prototype.

--
The change in 0d2db8b81ab24e2ab02d7ba6832cabd07b72f852
introduced a big problem in the check for Mail::close
which then allowed writes to pass that should have
been prevented. This resulted in plaintext being written.

src/mail.cpp
@@ -405,37 +405,11 @@ Mail::checkAttachments_o (bool silent)
                  SRCNAME, __func__);
       TRETURN 1;
     }
-  int count = get_oom_int (attachments, "Count");
+  int count = count_visible_attachments (attachments);
   if (!count)
     {
       gpgol_release (attachments);
       TRETURN 0;
-    }
-
-  /* Saveguard not to warn about our own attachment */
-  if (count == 1)
-    {
-      LPDISPATCH oom_attach = get_oom_object (attachments, "Item(1)");
-      if (oom_attach)
-        {
-          char *dispName = get_oom_string (oom_attach, "DisplayName");
-          gpgol_release (oom_attach);
-
-          if (dispName && !strcmp (dispName, MIMEATTACHFILENAME))
-            {
-              xfree (dispName);
-              gpgol_release (attachments);
-              log_debug ("%s:%s: Found only our hidden mime structure.",
-                         SRCNAME, __func__);
-              TRETURN 0;
-            }
-          else if (dispName)
-            {
-              log_debug ("%s:%s: Found %s as attachment.",
-                         SRCNAME, __func__, anonstr (dispName));
-              xfree (dispName);
-            }
-        }
     }
 
   std::string message;
@@ -2288,6 +2262,7 @@ Mail::closeInspector_o (Mail *mail)
   TRETURN 0;
 }
 
+
 int
 Mail::close (bool restoreSMIMEClass)
 {
@@ -2451,57 +2426,24 @@ Mail::close (bool restoreSMIMEClass)
        * */
       char *body = get_oom_string (m_mailitem, "Body");
       LPDISPATCH attachments = get_oom_object (m_mailitem, "Attachments");
-      int att_count = 0;
-      if (attachments)
-        {
-          att_count = get_oom_int (attachments, "Count");
-        }
 
-      bool foundOne = false;
       if (body && strlen (body))
         {
           log_debug ("%s:%s: Close successful. But body found. "
                      "Mail still open.",
                      SRCNAME, __func__);
         }
-      else if (att_count)
+      else if (count_visible_attachments (attachments))
         {
-          for (int i = 1; i <= att_count && !foundOne; i++)
-            {
-              std::string item_str;
-              item_str = std::string("Item(") + std::to_string (i) + ")";
-              LPDISPATCH oom_attach = get_oom_object (attachments, item_str.c_str ());
-              if (!oom_attach)
-                {
-                  log_error ("%s:%s: Failed to get attachment.",
-                             SRCNAME, __func__);
-                  continue;
-                }
-              VARIANT var;
-              VariantInit (&var);
-              if (get_pa_variant (oom_attach, PR_ATTACHMENT_HIDDEN_DASL, &var) ||
-                  (var.vt == VT_BOOL && var.boolVal == VARIANT_FALSE))
-                {
-                  foundOne = true;
-                }
-              else
-                {
-                  gpgol_release (oom_attach);
-                }
-              VariantClear (&var);
-            }
-          if (foundOne)
-            {
               log_debug ("%s:%s: Close successful. But attachments found. "
                          "Mail still open.",
                          SRCNAME, __func__);
-            }
         }
-      if (!foundOne)
+      else
         {
-          setPassWrite (true);
-          log_debug ("%s:%s: Close successful. Next write may pass.",
-                     SRCNAME, __func__);
+           setPassWrite (true);
+           log_debug ("%s:%s: Close successful. Next write may pass.",
+                      SRCNAME, __func__);
         }
       gpgol_release (attachments);
       xfree (body);

src/oomhelp.cpp
@@ -3095,3 +3095,41 @@ format_dispparams (DISPPARAMS *p)
     }
   return stream.str ();
 }
+
+int
+count_visible_attachments (LPDISPATCH attachments)
+{
+  int ret = 0;
+
+  if (!attachments)
+    {
+      return 0;
+    }
+
+  int att_count = get_oom_int (attachments, "Count");
+  for (int i = 1; i <= att_count; i++)
+    {
+      std::string item_str;
+      item_str = std::string("Item(") + std::to_string (i) + ")";
+      LPDISPATCH oom_attach = get_oom_object (attachments, item_str.c_str ());
+      if (!oom_attach)
+        {
+          log_error ("%s:%s: Failed to get attachment.",
+                     SRCNAME, __func__);
+          continue;
+        }
+      VARIANT var;
+      VariantInit (&var);
+      if (get_pa_variant (oom_attach, PR_ATTACHMENT_HIDDEN_DASL, &var) ||
+          (var.vt == VT_BOOL && var.boolVal == VARIANT_FALSE))
+        {
+           ret++;
+        }
+      else
+        {
+          gpgol_release (oom_attach);
+        }
+      VariantClear (&var);
+    }
+  return ret;
+}

src/oomhelp.h
@@ -440,4 +440,7 @@ bool is_draft_mail (LPDISPATCH mailitem);
 /* Returns info about a dispparms variable for debugging. */
 void format_variant (std::istringstream &stream, VARIANT *var);
 std::string format_dispparams (DISPPARAMS *p);
+
+/* Returns the count of attachments that are not hidden. */
+int count_visible_attachments (LPDISPATCH attachments);
 #endif /*OOMHELP_H*/