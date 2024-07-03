dee47a4c450435fb78e63a51af7f3f307375a4db
Werner Koch <wk@gnupg.org>
2024-07-03 12:08:29 +0200

    Fix indentation.
    * src/mimedataprovider.cpp (t2body): Just to be really sure check
    ctmain and ctsub before strcmp.
    --
    
    The check is not really required because at that point boths vars are
    not NULL.  However, it makes reading the code easier because there is
    no need to think about this.

--- src/mimedataprovider.cpp
+++ src/mimedataprovider.cpp
@@ -402,10 +402,11 @@ t2body (MimeDataProvider *provider, rfc822parse_t msg)
          was not found */
       filename = rfc2231_query_parameter (field, "name");
 
-      if (!filename && !strcmp(ctmain, "message")&& !strcmp(ctsub,"rfc822"))
-      {
-        filename = xstrdup("rfc822_email.eml");
-      }
+      if (!filename && ctmain && ctsub
+          && !strcmp (ctmain, "message") && !strcmp (ctsub, "rfc822"))
+        {
+          filename = xstrdup ("rfc822_email.eml");
+        }
     }
 
   /* Parse a Content Id header */
