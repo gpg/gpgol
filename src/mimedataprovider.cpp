3a1614bf140c85a5b4139d6b2e5fb09e01d1ea13
Andre Heinecke <aheinecke@gnupg.org>
2024-08-21 13:56:56 +0200

    Revert "Set missing filename to rfc822_email.eml..
    This reverts commit 0d16049d41e052ea9b5d7a7878ece3d02a182161.
    
    Since this fix only prints a static filename when an
    enacapsulated message is detected but does neither parase the
    mail nor fill it with the complete data it is better to
    leave it out for a bugfix release as this only creates
    more problems then it solves. The encapsulated message part
    needs to be eihter completely cut out from our parser and
    treated as a MAPI attachment or ignored if we do not support
    it.

--- src/mimedataprovider.cpp
+++ src/mimedataprovider.cpp
@@ -401,12 +401,6 @@ t2body (MimeDataProvider *provider, rfc822parse_t msg)
       /* Check for Content-Type name if Content-Disposition filename
          was not found */
       filename = rfc2231_query_parameter (field, "name");
-
-      if (!filename && ctmain && ctsub
-          && !strcmp (ctmain, "message") && !strcmp (ctsub, "rfc822"))
-        {
-          filename = xstrdup ("rfc822_email.eml");
-        }
     }
 
   /* Parse a Content Id header */
