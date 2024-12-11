gpg/gpgol

commit 9f81ed6561c5f41e50d1a51333c9586a33ed2ef6

Change encryption to work on OOM
* src/attachment.cpp (Attachment::Attachment(LPDISPATCH)): New.
Construct an in memory representation of an attachment object.
(Attachment::get_file_name): New, sometimes it differs from
display name.
(Attachment::copy_to): Move logic here from mail.cpp
(Attachment::olAttachmentType): New enum.
* src/cryptcontroller.cpp (CryptController::collect_data): Move
out attachment handling. Use new data from mail object.
* src/mail.cpp (copy_attachment_to_file): Moved into attachment
class.
(Mail::updateOOMData_o): Collect data of attachments, write
out mail to tmp file to do serialization.
(Mail::checkSyncCrypto_o): For now disable sync for attachments.
(Mail::isMultipartRelated): New. Moved from mimemaker.
(Mail::plainAttachments): New data object to carry attachment data.
* src/mailitem-events.cpp (SEND): Directly encrypt. Don't wait
for AfterWrite.
* src/main.c (read_options): Remove syncEnc override.
* src/mimemaker.cpp (write_attachments): Use mail object.
(add_body_and_attachments): Use mail object.

--
This fixes
GnuPG-Bug-Id: T5022
by working only on the outlook object model and avoiding
a sync to MAPI which might cause a server sync depending
on the MAPIStore implementation.

This will also help us to allow attaching mails and other
outlook objects because we now have better control about this.

src/attachment.cpp
@@ -22,13 +22,72 @@
 #include "config.h"
 #include "common_indep.h"
 #include "attachment.h"
+#include "mymapitags.h"
 
 #include <climits>
+
+#define COPYBUFSIZE (8 * 1024)
 
 Attachment::Attachment()
 {
   memdbg_ctor ("Attachment");
 }
+
+#ifndef BUILD_TESTS
+Attachment::Attachment(LPDISPATCH attach)
+{
+  memdbg_ctor ("Attachment");
+  if (!attach)
+    {
+      STRANGEPOINT;
+      TRETURN;
+    }
+  int type = get_oom_int (attach, "Type");
+  m_utf8DisplayName = get_oom_string (attach, "DisplayName");
+  m_fileName = get_oom_string (attach, "FileName");
+
+  log_dbg ("Creating attachment obj for type: %i for %s",
+           type, m_utf8DisplayName.c_str ());
+  if (type == olByValue)
+    {
+      log_dbg ("Copying attachment by value");
+      LPATTACH mapi_attachment = nullptr;
+      mapi_attachment = (LPATTACH) get_oom_iunknown (attach,
+                                                     "MapiObject");
+      if (!mapi_attachment)
+        {
+          log_debug ("%s:%s: Failed to get MapiObject of attachment: %p",
+                     SRCNAME, __func__, attach);
+          TRETURN;
+        }
+      LPSTREAM stream = nullptr;
+      if (FAILED (gpgol_openProperty (mapi_attachment, PR_ATTACH_DATA_BIN,
+                                      &IID_IStream, 0, MAPI_MODIFY,
+                                      (LPUNKNOWN*) &stream)))
+        {
+          log_debug ("%s:%s: Failed to open stream for mapi_attachment: %p",
+                     SRCNAME, __func__, mapi_attachment);
+          gpgol_release (mapi_attachment);
+          TRETURN;
+        }
+      gpgol_release (mapi_attachment);
+      char buf[COPYBUFSIZE];
+      HRESULT hr;
+      ULONG bRead = 0;
+      while ((hr = stream->Read (buf, 8192, &bRead)) == S_OK ||
+             hr == S_FALSE)
+        {
+          if (!bRead)
+            {
+              // EOF
+              break;
+            }
+          m_data.write (buf, bRead);
+        }
+      gpgol_release (stream);
+    }
+}
+#endif
 
 Attachment::~Attachment()
 {
@@ -52,6 +111,16 @@ Attachment::set_display_name(const char *name)
       return;
     }
   m_utf8DisplayName = std::string(name);
+}
+
+std::string
+Attachment::get_file_name () const
+{
+  if (m_fileName.size ())
+    {
+      return m_fileName;
+    }
+  return m_utf8DisplayName;
 }
 
 void
@@ -88,4 +157,49 @@ std::string
 Attachment::get_content_type() const
 {
   return m_ctype;
+}
+
+int
+Attachment::copy_to (HANDLE hFile)
+{
+  TSTART;
+  char copybuf[COPYBUFSIZE];
+  size_t nread;
+
+  /* Security considerations: Writing the data to a temporary
+     file is necessary as neither MAPI manipulation works in the
+     read event to transmit the data nor Property Accessor
+     works (see above). From a security standpoint there is a
+     short time where the temporary files are on disk. Tempdir
+     should be protected so that only the user can read it. Thus
+     we have a local attack that could also take the data out
+     of Outlook. FILE_SHARE_READ is necessary so that outlook
+     can read the file.
+
+     A bigger concern is that the file is manipulated
+     by another software to fake the signature state. So
+     we keep the write exlusive to us.
+
+     We delete the file before closing the write file handle.
+  */
+
+  /* Make sure we start at the beginning */
+  m_data.seek (0, SEEK_SET);
+  while ((nread = m_data.read (copybuf, COPYBUFSIZE)))
+    {
+      DWORD nwritten;
+      if (!WriteFile (hFile, copybuf, nread, &nwritten, NULL))
+        {
+          log_error ("%s:%s: Failed to write in tmp attachment.",
+                     SRCNAME, __func__);
+          TRETURN 1;
+        }
+      if (nread != nwritten)
+        {
+          log_error ("%s:%s: Write truncated.",
+                     SRCNAME, __func__);
+          TRETURN 1;
+        }
+    }
+  TRETURN 0;
 }

src/attachment.h
@@ -25,10 +25,20 @@
 
 #include <gpgme++/data.h>
 
+#ifdef _WIN32
+# include "oomhelp.h"
+#endif
 /** Helper class for attachment actions. */
 class Attachment
 {
 public:
+  enum olAttachmentType
+    {
+      olByValue = 1,
+      olByReference = 4,
+      olEmbeddeditem = 5,
+      olOLE = 6,
+    };
   /** Creates and opens a new in memory attachment. */
   Attachment();
   ~Attachment();
@@ -36,6 +46,9 @@ public:
   /** Set the display name */
   void set_display_name(const char *name);
   std::string get_display_name() const;
+
+  /** Get file name, usually the same as display name */
+  std::string get_file_name() const;
 
   void set_attach_type(attachtype_t type);
 
@@ -50,9 +63,20 @@ public:
   /* get the underlying data structure */
   GpgME::Data& get_data();
 
+  /** Check if this attachment is bound to an OOM object */
+  bool fromOOM () const;
+
+#ifdef _WIN32
+  /** Create a data struct from OOM attachment */
+  Attachment (LPDISPATCH attach);
+
+  /** Copy the attachment to a file handle */
+  int copy_to (HANDLE hFile);
+#endif
 private:
   GpgME::Data m_data;
   std::string m_utf8DisplayName;
+  std::string m_fileName;
   attachtype_t m_type;
   std::string m_cid;
   std::string m_ctype;

src/cryptcontroller.cpp
@@ -91,13 +91,7 @@ int
 CryptController::collect_data ()
 {
   TSTART;
-  /* Get the attachment info and the body.  We need to do this before
-     creating the engine's filter because sending the cancel to
-     the engine with nothing for the engine to process.  Will result
-     in an error. This is actually a bug in our engine code but
-     we better avoid triggering this bug because the engine
-     sometimes hangs.  Fixme: Needs a proper fix. */
-
+  int count = m_mail->plainAttachments ().size ();
 
   /* Take the Body from the mail if possible. This is a fix for
      GnuPG-Bug-ID: T3614 because the body is not always properly
@@ -108,19 +102,7 @@ CryptController::collect_data ()
       xfree (body);
       body = nullptr;
     }
-
-  LPMESSAGE message = m_mail->isCryptoMail() ?
-                      get_oom_base_message (m_mail->item ()) :
-                      get_oom_message (m_mail->item ());
-  if (!message)
-    {
-      log_error ("%s:%s: Failed to get message.",
-                 SRCNAME, __func__);
-    }
-
-  auto att_table = mapi_create_attach_table (message, 0);
-  int n_att_usable = count_usable_attachments (att_table);
-  if (!n_att_usable && !body)
+  if (!count && !body)
     {
       if (!m_mail->isDraftEncrypt())
         {
@@ -128,15 +110,13 @@ CryptController::collect_data ()
                              utf8_gettext ("Can't encrypt / sign an empty message."),
                              utf8_gettext ("GpgOL"), MB_OK);
         }
-      gpgol_release (message);
-      mapi_release_attach_table (att_table);
       xfree (body);
       TRETURN -1;
     }
 
   bool do_inline = m_mail->getDoPGPInline ();
 
-  if (n_att_usable && do_inline)
+  if (count && do_inline)
     {
       log_debug ("%s:%s: PGP Inline not supported for attachments."
                  " Using PGP MIME",
@@ -164,23 +144,15 @@ CryptController::collect_data ()
   sink->writefnc = sink_data_write;
 
   /* Collect the mime strucutre */
-  int err = add_body_and_attachments (sink, message, att_table, m_mail,
-                                      body, n_att_usable);
+  int err = add_body_and_attachments (sink, m_mail, body);
   xfree (body);
 
   if (err)
     {
       log_error ("%s:%s: Collecting body and attachments failed.",
                  SRCNAME, __func__);
-      gpgol_release (message);
-      mapi_release_attach_table (att_table);
       TRETURN -1;
     }
-
-  /* Message is no longer needed */
-  gpgol_release (message);
-
-  mapi_release_attach_table (att_table);
 
   /* Set the input buffer to start. */
   m_input.seek (0, SEEK_SET);

src/mail.cpp
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2015, 2016 by Bundesamt für Sicherheit in der Informationstechnik
  * Software engineering by Intevation GmbH
- * Copyright (C) 2019 g10code GmbH
+ * Copyright (C) 2019, 2020 g10code GmbH
  *
  * This file is part of GpgOL.
  *
@@ -96,7 +96,6 @@ const char *HTML_PREVIEW_PLACEHOLDER =
 "</table></body></html>";
 
 const char *TEXT_PREVIEW_PLACEHOLDER = "%s %s %s\n\n%s";
-#define COPYBUFSIZE (8 * 1024)
 
 Mail::Mail (LPDISPATCH mailitem) :
     m_mailitem(mailitem),
@@ -690,51 +689,6 @@ copy_data_property(LPDISPATCH target, std::shared_ptr<Attachment> attach)
 }
 #endif
 
-static int
-copy_attachment_to_file (std::shared_ptr<Attachment> att, HANDLE hFile)
-{
-  TSTART;
-  char copybuf[COPYBUFSIZE];
-  size_t nread;
-
-  /* Security considerations: Writing the data to a temporary
-     file is necessary as neither MAPI manipulation works in the
-     read event to transmit the data nor Property Accessor
-     works (see above). From a security standpoint there is a
-     short time where the temporary files are on disk. Tempdir
-     should be protected so that only the user can read it. Thus
-     we have a local attack that could also take the data out
-     of Outlook. FILE_SHARE_READ is necessary so that outlook
-     can read the file.
-
-     A bigger concern is that the file is manipulated
-     by another software to fake the signature state. So
-     we keep the write exlusive to us.
-
-     We delete the file before closing the write file handle.
-  */
-
-  /* Make sure we start at the beginning */
-  att->get_data ().seek (0, SEEK_SET);
-  while ((nread = att->get_data ().read (copybuf, COPYBUFSIZE)))
-    {
-      DWORD nwritten;
-      if (!WriteFile (hFile, copybuf, nread, &nwritten, NULL))
-        {
-          log_error ("%s:%s: Failed to write in tmp attachment.",
-                     SRCNAME, __func__);
-          TRETURN 1;
-        }
-      if (nread != nwritten)
-        {
-          log_error ("%s:%s: Write truncated.",
-                     SRCNAME, __func__);
-          TRETURN 1;
-        }
-    }
-  TRETURN 0;
-}
-
 /** Sets some meta data on the last attachment added. The meta
   data is taken from the attachment object. */
 static int
@@ -878,7 +832,7 @@ Mail::add_attachments_o (std::vector<std::shared_ptr<Attachment> > attachments)
                      SRCNAME, __func__, anonstr (dispName.c_str()));
           err = 1;
         }
-      if (!err && copy_attachment_to_file (att, hFile))
+      if (!err && att->copy_to (hFile))
         {
           log_error ("%s:%s: Failed to copy attachment %s to temp file",
                      SRCNAME, __func__, anonstr (dispName.c_str()));
@@ -1068,12 +1022,14 @@ do_parsing (LPVOID arg)
       mail->update_oom_data
       State = Mail::NeedsFirstAfterWrite
       checkSyncCrypto_o
-      invoke_oom_method (m_object, "Save", NULL);
+        sync ->
+          invoke_oom_method (m_object, "Save", NULL);
 
-      > Write Event <
-      Pass because is_crypto_mail is false (not a decrypted mail)
+          > Write Event <
+          Pass because is_crypto_mail is false (not a decrypted mail)
 
-      > AfterWrite Event < | State NeedsFirstAfterWrite
+          > AfterWrite Event < | State NeedsFirstAfterWrite
+        async ->  skips ^
       State = NeedsActualCrypo
       encrypt_sign_start
         collect_input_data
@@ -2156,6 +2112,50 @@ Mail::updateOOMData_o (bool for_encryption)
 
   if (for_encryption)
     {
+      /* Write out the mail to a temporary file so that all the
+         properties are set correctly. */
+
+      HANDLE hTmpFile = INVALID_HANDLE_VALUE;
+      char *path = get_tmp_outfile_utf8 ("gpgol_enc.dat", &hTmpFile);
+      if (!path || hTmpFile == INVALID_HANDLE_VALUE)
+        {
+          STRANGEPOINT;
+          TRETURN -1;
+        }
+      CloseHandle (hTmpFile);
+
+      if (oom_save_as (m_mailitem, path, 3))
+        {
+          log_dbg ("Failed to call SaveAs.");
+          TRETURN -1;
+        }
+
+      wchar_t *wpath = utf8_to_wchar (path);
+      if (FAILED (DeleteFileW (wpath)))
+        {
+          log_err ("Failed to delete file %S", wpath);
+        }
+      xfree (wpath);
+
+      /* Clear any leftovers */
+      m_plain_attachments.clear ();
+
+      auto attachments = get_oom_object_s (m_mailitem, "Attachments");
+      int count = attachments ? get_oom_int (attachments, "Count") : 0;
+
+      for (int i = 1; i <= count; i++)
+        {
+          std::string item = asprintf_s ("Item(%i)", i);
+          LPDISPATCH attach = get_oom_object (attachments.get (), item.c_str ());
+          if (!attach)
+            {
+              STRANGEPOINT;
+              continue;
+            }
+          m_plain_attachments.push_back (std::shared_ptr <Attachment> (
+                                         new Attachment (attach)));
+        }
+
       /* Update the body format. */
       m_is_html_alternative = get_oom_int (m_mailitem, "BodyFormat") > 1;
 
@@ -4229,7 +4229,7 @@ Mail::checkSyncCrypto_o ()
     }
 
   LPDISPATCH attachments = get_oom_object (m_mailitem, "Attachments");
-  if (attachments)
+  if (/* attachments */ false)
     {
       /* This is horrible. But. For some kinds of attachments (we
          got reports about Office attachments the write in the
@@ -5164,4 +5164,29 @@ const std::string&
 Mail::getOriginalBody () const
 {
   return m_orig_body;
+}
+
+int
+Mail::isMultipartRelated () const
+{
+  int related = false;
+  int mixed = false;
+  for (const auto attach: m_plain_attachments)
+    {
+      if (attach->get_content_id ().size())
+        {
+          related = 1;
+        }
+      else
+        {
+          mixed = 1;
+        }
+    }
+  return related + mixed;
+}
+
+std::vector <std::shared_ptr<Attachment> >
+Mail::plainAttachments () const
+{
+  return m_plain_attachments;
 }

src/mail.h
@@ -701,6 +701,15 @@ public:
 
   /* Get the original body of the mail. */
   const std::string &getOriginalBody () const;
+
+  /* Get the plain attachments for an encrypt / sign operation. Set through
+     updateOOMData_o */
+  std::vector <std::shared_ptr <Attachment> > plainAttachments() const;
+
+  /* True if this is a multipart related mail, 1 if all attachments are
+     related, 2 if there is a related and a mixed attachment. */
+  int isMultipartRelated () const;
+
 private:
   /* Returns a copy of the mailitem object. This copy
      is sadly not the same as in the ItemLoad event
@@ -773,6 +782,7 @@ private:
   header_info_s m_header_info; /* Information about the original headers */
   bool m_attachs_added; /* State variable to track if we have added attachments to this mail. */
   std::string m_dec_content_type; /* Top level content type of the decrypted mail. */
+  std::vector <std::shared_ptr <Attachment> > m_plain_attachments; /* Attachments to encrypt */
 };
 
 /* A state variable to capture which mail triggered a copy to

src/mailitem-events.cpp
@@ -1,6 +1,7 @@
 /* mailitem-events.h - Event handling for mails.
  * Copyright (C) 2015 by Bundesamt für Sicherheit in der Informationstechnik
  * Software engineering by Intevation GmbH
+ * Copyright (C) 2019, 2020 g10code GmbH
  *
  * This file is part of GpgOL.
  *
@@ -457,19 +458,28 @@ EVENT_SINK_INVOKE(MailItemEvents)
                 }
               m_mail->setIsDraftEncrypt (false);
 
-              // Save the Mail
-              invoke_oom_method (m_object, "Save", NULL);
-
               if (!m_mail->isAsyncCryptDisabled ())
                 {
-                  // The afterwrite in the save should have triggered
-                  // the encryption. We cancel send for our asyncness.
-                  // Cancel send
+                  /* The afterwrite in the save should have triggered
+                     the encryption. We cancel send for our asyncness. */
                   *(parms->rgvarg[0].pboolVal) = VARIANT_TRUE;
+
+                  /* Do the crypto operation */
+                  m_mail->setCryptState (Mail::NeedsActualCrypt);
+                  if (m_mail->encryptSignStart_o ())
+                    {
+                      log_debug ("%s:%s: Encrypt sign start failed.",
+                                 SRCNAME, __func__);
+                      m_mail->setCryptState (Mail::NoCryptMail);
+                      m_mail->releaseCurrentItem();
+                    }
                   TBREAK;
                 }
               else
                 {
+                  // Save the Mail
+                  invoke_oom_method (m_object, "Save", NULL);
+
                   if (m_mail->cryptState () == Mail::NoCryptMail)
                     {
                       // Crypto failed or was canceled

src/main.c
@@ -338,7 +338,7 @@ read_options (void)
   opt.alwaysShowApproval = get_conf_bool ("alwaysShowApproval", 0);
 
   /* Hidden options  */
-  opt.sync_enc = 1; //get_conf_bool ("syncEnc", 0);
+  opt.sync_enc = get_conf_bool ("syncEnc", 0);
   /* Due to an issue where async encryption would leave
      unencrypted mails in the recently deleted folder on the
      server we block it. */

src/mimemaker.cpp
@@ -42,6 +42,8 @@
 #include "mimemaker.h"
 #include "oomhelp.h"
 #include "mail.h"
+#include "attachment.h"
+#include "cpphelp.h"
 
 #undef _
 #define _(a) utf8_gettext (a)
@@ -1025,115 +1027,59 @@ count_usable_attachments (mapi_attach_item_t *table)
    content-id. */
 static int
 write_attachments (sink_t sink,
-                   LPMESSAGE message, mapi_attach_item_t *table,
+                   Mail *mail,
                    const char *boundary, int only_related)
 {
-  int idx, rc;
-  char *buffer;
-  size_t buflen;
+  TSTART;
+  int rc;
   bool warning_shown = false;
-
-  if (table)
-    for (idx=0; !table[idx].end_of_table; idx++)
-      {
-        if (table[idx].attach_type == ATTACHTYPE_UNKNOWN
-            && table[idx].method == ATTACH_BY_VALUE)
-          {
-            if (only_related == 1 && !table[idx].content_id)
-              {
-                continue;
-              }
-            else if (!only_related && table[idx].content_id)
-              {
-                continue;
-              }
-            buffer = mapi_get_attach (message, table+idx, &buflen);
-            if (!buffer)
-              log_debug ("Attachment at index %d not found\n", idx);
-            else
-              log_debug ("Attachment at index %d: length=%d\n", idx, (int)buflen);
-            if (!buffer)
-              return -1;
-            rc = write_part (sink, buffer, buflen, boundary,
-                             table[idx].filename, 0, table[idx].content_id);
-            if (rc)
-              {
-                log_error ("Write part returned err: %i", rc);
-              }
-            xfree (buffer);
-          }
-        else if (!only_related && !warning_shown
-                && table[idx].attach_type == ATTACHTYPE_UNKNOWN
-                && (table[idx].method == ATTACH_OLE
-                    || table[idx].method == ATTACH_EMBEDDED_MSG))
-          {
-            char *fmt;
-            log_debug ("%s:%s: detected OLE attachment. Showing warning.",
-                       SRCNAME, __func__);
-            gpgrt_asprintf (&fmt, _("The attachment '%s' is an Outlook item "
-                                    "which is currently unsupported in crypto mails."),
-                            table[idx].filename ?
-                            table[idx].filename : _("Unknown"));
-            std::string msg = fmt;
-            msg += "\n\n";
-            xfree (fmt);
-
-            gpgrt_asprintf (&fmt, _("Please encrypt '%s' with Kleopatra "
-                                    "and attach it as a file."),
-                            table[idx].filename ?
-                            table[idx].filename : _("Unknown"));
-            msg += fmt;
-            xfree (fmt);
-
-            msg += "\n\n";
-            msg += _("Send anyway?");
-            warning_shown = true;
-
-            if (gpgol_message_box (get_active_hwnd (),
-                                   msg.c_str (),
-                                   _("Sorry, that's not possible, yet"),
-                                   MB_APPLMODAL | MB_YESNO) == IDNO)
-              {
-                return -1;
-              }
-          }
-        else
-          {
-            log_debug ("%s:%s: Skipping unknown attachment at idx: %d type: %d"
-                       " with method: %d",
-                       SRCNAME, __func__, idx, table[idx].attach_type,
-                       table[idx].method);
-          }
-      }
-  return 0;
-}
-
-/* Returns 1 if all attachments are related. 2 if there is a
-   related and a mixed attachment. 0 if there are no other parts*/
-static int
-is_related (Mail *mail, mapi_attach_item_t *table)
-{
-  if (!mail || !mail->isHTMLAlternative () || !table)
+  for (auto attach: mail->plainAttachments ())
     {
-      return 0;
-    }
-
-  int related = 0;
-  int mixed = 0;
-  for (int idx = 0; !table[idx].end_of_table; idx++)
-    {
-      if (table[idx].content_id)
+      std::string cid = attach->get_content_id ();
+      if (only_related == 1 && !cid.size ())
         {
-          related = 1;
+          continue;
         }
-      else
+      else if (!only_related && cid.size ())
         {
-          mixed = 1;
+          continue;
+        }
+      std::string buf = attach->get_data ().toString ();
+      const auto name = attach->get_file_name ();
+      if (!warning_shown && !only_related && !buf.size ())
+        {
+          log_debug ("%s:%s: detected OLE attachment. Showing warning.",
+                     SRCNAME, __func__);
+          std::string msg = asprintf_s (_("The attachment '%s' is an Outlook item "
+                                          "which is currently unsupported in crypto mails."),
+                                          name.size () ?
+                                          name.c_str (): _("Unknown"));
+          msg += "\n\n";
+          msg += asprintf_s (_("Please encrypt '%s' with Kleopatra "
+                               "and attach it as a file."),
+                               name.size () ?
+                               name.c_str (): _("Unknown"));
+          msg += "\n\n";
+          msg += _("Send anyway?");
+          warning_shown = true;
+
+          if (gpgol_message_box (get_active_hwnd (),
+                                 msg.c_str (),
+                                 _("Sorry, that's not possible, yet"),
+                                 MB_APPLMODAL | MB_YESNO) == IDNO)
+            {
+              TRETURN -1;
+            }
+        }
+      rc = write_part (sink, buf.c_str (), buf.size (), boundary,
+                       name.c_str (), 0, attach->get_content_id ().c_str ());
+      if (rc)
+        {
+          log_error ("Write part returned err: %i", rc);
         }
     }
-  return mixed + related;
+  TRETURN 0;
 }
-
 
 /* Delete all attachments from TABLE except for the one we just created */
 static int
@@ -1465,16 +1411,20 @@ add_body (Mail *mail, const char *boundary, sink_t sink,
 
 /* Add the body and attachments. Does multipart handling. */
 int
-add_body_and_attachments (sink_t sink, LPMESSAGE message,
-                          mapi_attach_item_t *att_table, Mail *mail,
-                          const char *body, int n_att_usable)
+add_body_and_attachments (sink_t sink, Mail *mail, const char *body)
 {
-  int related = is_related (mail, att_table);
+  if (!mail)
+    {
+      STRANGEPOINT;
+      TRETURN -1;
+    }
+  int related = mail->isMultipartRelated ();
   int rc = 0;
   char inner_boundary[BOUNDARYSIZE+1];
   char outer_boundary[BOUNDARYSIZE+1];
   *outer_boundary = 0;
   *inner_boundary = 0;
+  int n_att_usable = mail->plainAttachments ().size ();
 
   if (((body && n_att_usable) || n_att_usable > 1) && related == 1)
     {
@@ -1530,7 +1480,7 @@ add_body_and_attachments (sink_t sink, LPMESSAGE message,
   if (!rc && n_att_usable && related)
     {
       /* Write the related attachments. */
-      rc = write_attachments (sink, message, att_table,
+      rc = write_attachments (sink, mail,
                               *inner_boundary? inner_boundary :
                               *outer_boundary? outer_boundary : NULL, 1);
       if (rc)
@@ -1556,7 +1506,7 @@ add_body_and_attachments (sink_t sink, LPMESSAGE message,
      */
   if (!rc && n_att_usable)
     {
-      rc = write_attachments (sink, message, att_table,
+      rc = write_attachments (sink, mail,
                               *outer_boundary? outer_boundary : NULL,
                               related ? 0 : 2);
     }

commit a7349189f3af05822eba4bd17b62482fa2b0747f

Fix sending multipart/related
This fixes writing the content-id for attachments in
MIME which was broken with
9f81ed6561c5f41e50d1a51333c9586a33ed2ef6

While this issue was hidden when we had related attachments
disabled due to T5982 it would also not work
in third party clients.

Fixes-Commit: 9f81ed6561c5f41e50d1a51333c9586a33ed2ef6
GnuPG-Bug-Id: T5982

src/attachment.cpp
@@ -47,6 +47,12 @@ Attachment::Attachment(LPDISPATCH attach)
   int type = get_oom_int (attach, "Type");
   m_utf8DisplayName = get_oom_string_s (attach, "DisplayName");
   m_fileName = get_oom_string_s (attach, "FileName");
+  char *cid = get_pa_string (attach, PR_ATTACH_CONTENT_ID_DASL);
+  if (cid)
+    {
+      m_cid = cid;
+      xfree (cid);
+    }
 
   if (m_fileName.empty())
     {